d4a585b55414a0868334130a32556aa46e8b87e4
Daniel <davilareko@gmail.com>
2022-03-19 16:06:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,32 +24,10 @@ function download(url, dest, cb) {
 }
 
 /**
- * @param cmd - The command to run.
- * @param cb - Callback function to be executed when the command is finished.
+ * *This function deletes the cached version of the module and then returns the module. 
+ * This is useful if you want to force a refresh of the module.*
+ * @param {string} module - The name of the module to be required.
  */
-async function RunCmd(cmd, cb) {
-    const syncDir = await execute(cmd);
-    cb;
-}
-
-/**
- * It executes a command in the shell and returns the output.
- * @param command - The command to execute.
- * @returns The output of the command.
- */
-async function execute(command) {
-    const { promisify } = require('util');
-    const exec = promisify(require('child_process').exec)
-    // Exec output contains both stderr and stdout outputs
-    try {
-        const nameOutput = await exec(command, { windowsHide: true, stdio: 'inherit' })
-        return nameOutput.stdout;
-    }
-    catch (e) {
-        return e.stderr;
-    }
-};
-
 function requireUncached(module) {
     delete require.cache[require.resolve(module)];
     return require(module);
@@ -62,9 +40,7 @@ module.exports.Upgrade = function () {
     delete require.cache[require.resolve("./main.js")];
 
     download("https://raw.githubusercontent.com/Davilarek/LinuxJSEdition/master/main.js", "./main.js", function () {
-        RunCmd("npm i", function () {
-            mainFile = requireUncached("./main.js");
-        });
+        mainFile = requireUncached("./main.js");
     });
 };
 
